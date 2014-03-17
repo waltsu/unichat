d31c33ba70bbea5b4f4e91aa8e586c5cf782dbd5
Petri Avikainen <petri.avikainen@gmail.com>
2014-03-17 15:02:21 +0200

    gulpfile and package json

---
+++ client/gulpfile.js
@@ -0,0 +1,86 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    livereload = require('gulp-livereload'),
+    browserify = require('browserify'),
+    coffeeify = require('coffeeify'),
+    sass = require('gulp-sass'),
+    source = require('vinyl-source-stream'),
+    streamify = require('gulp-streamify'),
+    lr = require('tiny-lr'),
+    server = lr(),
+    path = require('path'),
+    livereloadPort = 35729;
+
+
+var paths = {
+  scripts: ['app/**/*.coffee'],
+  styles: ['app/styles/**/*.scss']
+};
+
+// using vinyl-source-stream:
+compileCoffee = function (debug) {
+  var bundleStream = browserify('./app/main.coffee')
+    .transform('coffeeify')
+    .bundle()
+    .pipe(source('bundle.js'));
+  if (!debug) {
+    bundleStream.pipe(streamify(uglify()));
+  }
+  bundleStream
+    .pipe(gulp.dest('./public/js/'))
+    .pipe(livereload(server));
+};
+
+var compileStyles = function (opts) {
+  return gulp.src(paths.styles)
+    .pipe(sass(opts))
+    .on('error', function(err) {
+      console.log(err);
+    })
+    .pipe(gulp.dest('./public/styles'))
+    .pipe(livereload(server));
+};
+
+gulp.task('coffee', function () {
+  compileCoffee(true);
+});
+gulp.task('coffee-production', function () {
+  compileCoffee(false);
+});
+gulp.task('styles', function () {
+  compileStyles({sourceComments: 'map'});
+});
+gulp.task('styles-production', function () {
+  compileStyles({outputStyle: 'compressed'});
+});
+
+
+gulp.task('staticsvr', function(next) {
+  var staticS = require('node-static'),
+      server = new staticS.Server(),
+      port = 8000;
+  require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+      server.serve(request, response);
+    }).resume();
+  }).listen(port, function() {
+    console.log('Server listening on port: ' + port);
+    next();
+  });
+});
+
+gulp.task('livereload', ['staticsvr'], function () {
+  server.listen(livereloadPort, function (err) {
+    if (err) return console.log(err);
+  });
+});
+
+gulp.task('dev', function () {
+  gulp.start('livereload', ['coffee', 'styles']);
+  gulp.watch(paths.scripts, ['coffee']);
+  gulp.watch(paths.styles, ['styles']);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['coffee-production', 'styles-production']);
